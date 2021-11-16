5754478a45d2a2057fd2ee2f80882379713522fd
Harmanvir Singh <59752492+harmanvir1995@users.noreply.github.com>
2021-11-16 17:30:00 -0500

    Add files via upload

--- Car.java
+++ Car.java
@@ -1,53 +1,123 @@
 
-public class Car extends Vehicle{
-	private int wheels;
-	private int doors;
-	private int gears;
-	private boolean isManual;
-	private int currentGear;
+public class Car {
+	String make;
+	String model;
+	int year ;
+	String fuelType;
+	boolean isTransmissionAutomatic;
+	double price;
 	
-	public Car(String name, String size, int wheels, int doors, int gears, boolean isManual) {
-		super(name, size);
-		this.wheels = wheels;
-		this.doors = doors;
-		this.gears = gears;
-		this.isManual = isManual;
-		this.currentGear = 1;
+	
+	public Car(){
+		make = "";
+		model = "";
+		fuelType = "";
+		year = 2020;
+		isTransmissionAutomatic = true;
+		price = 0.0;
 	}
 	
-	public void setCurrentGear(int currentGear) {
-		this.currentGear = currentGear;
-		System.out.println("Current Gear of the vehicle has been set to " + currentGear);
+	public Car(String make, String model, int year){
+		this.make = make;
+		this.model = model;
+		this.year = year;
+		isTransmissionAutomatic = true;
+		fuelType = "";
 	}
 	
-	public String getCurrentGear() {
-		return "Current gear of the vehicle is "+ this.currentGear;
+	
+	public String getMakeName() {
+		return this.make;
 	}
 	
-	public void changeSpeed(int speed, int direction) {
-		move(speed, direction);
+	public String getModel() {
+		return this.model;
 	}
 	
-	public void changeGear(int gear) {
-		this.currentGear = gear;
+	public int getManufacturingYear() {
+		return this.year;
+	}
+	
+	public String getTransmissionType () {
+		if(true) {
+			return "automatic";
+		}
+		else {
+			return "manual";
+		}
+	}
+	
+	
+	public void setMakeName(String name) {
+		this.make = name;
+	}
+	
+	public void setModel(String name) {
+		this.model = name;
+	}
+	
+	public void setManufacturingYear(int year) {
+		this.year = year;
+	}
+
+	public void setTransmissionType(String transmissionType) {
+		if(transmissionType.equals("automatic")) {
+			this.isTransmissionAutomatic = true;
+		}
+		else if(transmissionType.equals("manual")) {
+			this.isTransmissionAutomatic = false;
+		}
+	}
+	
+	public void setFuelType (String fuelType) {
+		this.fuelType = fuelType;
+	}
+	
+
+	public void setPrice  (double price) {
+		this.price = price;
+	}
+	
+	
+	
+	public String toString() {
+		
+		return this.getMakeName() + " " + this.getModel() 
+		+ " " + this.getTransmissionType() + this.fuelType +
+		" manufactured in "+ this.getManufacturingYear() + " costs "+ this.price;
+	}
+	
+	public boolean equals(Car c) {
+		if(this.getMakeName().equals(c.getMakeName()) && 
+				this.getModel().equals(c.getModel()) &&
+				this.getManufacturingYear() == c.getManufacturingYear() &&
+				this.getTransmissionType() == c.getTransmissionType())
+		{
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	public boolean isComparable(Car c){
+		
+		
+		if(this.getTransmissionType() == c.getTransmissionType()
+				&& this.fuelType.equals(c.fuelType) 
+				&& c.price <= 0.1*this.price) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	
 	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
